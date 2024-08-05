4c33d1694c19e71105c2c3e00de5ffbc0a3e27cc
Dia Ledesma <dia81818181@gmail.com>
2024-08-05 22:31:01 +0000

    edited style of answers and javascript answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 document.getElementById('askButton').addEventListener('click', function() {
     const answers = [
-        "Certainly Yes",
-        "Definitely No",
-        "Maybe",
-        "It's likely",
-        "It's very possible",
-        "Not right now",
-        "Yes but later",
-        "For now yes",
-        "Yes, In a Distant Future",
-        "Not Likely",
-        "Sorry Not Really",
-        "Big Yes",
-        "Big No",
-        "Small No",
-        "Small Yes"
+        "Your answer is : Certainly yes",
+        "Your answer is : Definitely no",
+        "Your answer is : Maybe",
+        "Your answer is : It's likely",
+        "Your answer is : It's very possible",
+        "Your answer is : Not right now",
+        "Your answer is : Yes but later",
+        "Your answer is : For now yes",
+        "Your answer is : Yes, but in future",
+        "Your answer is : Not likely",
+        "Your answer is : Sorry not really",
+        "Your answer is : Big yes",
+        "Your answer is : Big no",
+        "Your answer is : Not a definite no",
+        "Your answer is : Not a definite yes"
     ];
 
     const randomIndex = Math.floor(Math.random() * answers.length);
